a161ee8842e88e00ceeac87fed59bdd75ae0612d
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-04-10 03:35:19 +0000

    Snapshot of branch master taken on 2019-04-09T23:35:18.686-04:00 from "https://github.com/UnofficialJuliaMirror/SpatialEcology.jl-348f2d5d-71a3-5ad4-b565-8af070f99681"

--- src/Operations.jl
+++ src/Operations.jl
@@ -13,20 +13,20 @@ not specified the default function is `any` for Boolean Assemblages and `sum` fo
 Integer ones.
 """
 aggregate(lo::SELocations, factor) = Locations{GridData}(aggregate(lo.coords, factor))
-aggregate(gr::GridTopology, factor) = GridTopology(gr.xs[1]:step(gr.xs)*factor:gr.xs[end], gr.ys[1]:step(gr.ys)*factor:gr.ys[end])
-aggregate(gr::SEGrid, factor::Integer) = (g = aggregate(gr.grid, factor); aggregate(gr, g))
-aggregate(gr::SEGrid, newgrid::GridTopology) = (ind = apply_grid(gr, newgrid); GridData(sortslices(unique(ind, dims = 1), dims = 1), newgrid))
+aggregate(gr::GridTopology, factor) = _grid_from_factor(gr, factor)
+aggregate(gr::SEGrid, factor::Integer) = (g = _grid_from_factor(gr.grid, factor); aggregate(gr, g))
+aggregate(gr::SEGrid, newgrid::GridTopology) = (ind = _apply_grid(gr, newgrid); GridData(sortslices(unique(ind, dims = 1), dims = 1), newgrid))
 
-function aggregate(asm::SEAssemblage{D, T, P}, factor::Integer, fun = _default_fun(D)) where D where T where P <: SELocations{<: SEGrid}
+function aggregate(asm::SEAssemblage{D, T, P}, factor::Union{Integer, Tuple{Integer, Integer}}, fun = _default_fun(D)) where D where T where P <: SELocations
 
-    gt = aggregate(asm.site.coords.grid, factor)
+    gt = _grid_from_factor(asm.site.coords, factor)
     aggregate(asm, gt, fun)
 end
 
-function aggregate(asm::SEAssemblage{D}, gt::GridTopology, fun = _default_fun(D)) where D
+function aggregate(asm::SEAssemblage{D}, gt::GridTopology, fun = _default_fun(D)) where D where T where P <: SELocations
 
     # create the new grid
-    tmpgrid = apply_grid(asm.site.coords, gt)
+    tmpgrid = _apply_grid(asm.site.coords, gt)
     tmpsites = sortslices(unique(tmpgrid, dims = 1), dims = 1)
 
     # fill a new occurrence matrix
@@ -44,13 +44,28 @@ function aggregate(asm::SEAssemblage{D}, gt::GridTopology, fun = _default_fun(D)
     Assemblage(sit,sp)
 end
 
-function apply_grid(gr::SEGrid, newgrid::GridTopology)
+#-----------
+# `aggregate` helper functions
+
+_lowerleft(gr) = (xmin(gr), ymin(gr)) .- (0.5 .* cellsize(gr))
+
+function _apply_grid(gr::SEGrid, newgrid::GridTopology)
     factor = [cellsize(newgrid)...] ./ cellsize(gr)
-    shift = [xmin(newgrid) - xmin(gr), ymin(newgrid) - ymin(gr)] .% factor ./cellsize(gr)
+    shift = (_lowerleft(gr) .- _lowerleft(newgrid)) .% factor ./cellsize(gr)
     newind = ceil.(Int, (shift' .+ gr.indices) ./ factor')
     newind
 end
-apply_grid(pt::SEPoints, newgrid::GridTopology) = getindices(coordinates(pt), newgrid)
+_apply_grid(pt::SEPoints, newgrid::GridTopology) = getindices(coordinates(pt), newgrid)
+
+_range_from_factor(mi, ma, factor) =
+    range((floor(mi / factor) + 0.5) * factor, stop = (ceil(ma/factor) - 0.5) * factor, step = factor)
+
+function _grid_from_factor(pt::SEPoints, factor)
+    (xmi, xma), (ymi, yma) = mapslices(extrema, coordinates(pt), dims = 1)
+    GridTopology(_range_from_factor(xmi, xma, first(factor)), _range_from_factor(ymi, yma, last(factor)))
+end
+_grid_from_factor(gd::SEGrid, factor) = _grid_from_factor(gd.grid, factor)
+_grid_from_factor(gd::GridTopology, factor) = GridTopology(_range_from_factor(extrema(gd.xs)..., first(factor)), _range_from_factor(extrema(gd.ys)..., last(factor)))
 
 _default_fun(::Type{Bool}) = Base.any
 _default_fun(::Type{Integer}) = Base.sum
